724252d97091c8c46d44bf2d3d6aa7e6a8dc3ea0
santhoshprofessional <santhosh22401@gmail.com>
2026-05-07 14:41:49 +0530

    chnage the main filr

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import * as dns from 'node:dns';
 import { AppModule } from './app.module';
-
 import { ZodValidationPipe } from 'nestjs-zod';
-
-import { DEFAULT_CORS_ORIGIN, DEFAULT_PORT } from './shared/constants/constants';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  DEFAULT_CORS_ORIGIN,
+  DEFAULT_PORT,
+} from './shared/constants/constants';
 
 async function bootstrap() {
   dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-  const app =
-    await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: DEFAULT_CORS_ORIGIN,
     credentials: true,
   });
 
-  app.useGlobalPipes(
-    new ZodValidationPipe(),
-  );
+  app.useGlobalPipes(new ZodValidationPipe());
 
- const port = Number(process.env.PORT) || DEFAULT_PORT;
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Coupon-Based Lead Form API')
+    .setDescription(
+      'API documentation for managing coupons and lead submissions',
+    )
+    .setVersion('1.0')
+    .addTag('coupons')
+    .addTag('leads')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = Number(process.env.PORT) || DEFAULT_PORT;
   await app.listen(port);
   console.log(`API running on http://localhost:${port}/`);
+  console.log(`Swagger Docs available on http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
